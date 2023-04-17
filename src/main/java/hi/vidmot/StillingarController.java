27523ad65f2c90f3064608f6c51b96044654d35d
Donn Eunice Bartido Patambag <Donneunice@hotmail.com>
2023-04-17 18:45:49 +0000

    stillingarogsena
    - breytti uppsetningu í fxml skrá
    - setti eh functions inni StillingarController
    - Þarf að forrita initialize dæmið til að fá göfn inn í senuna

--- src/main/java/hi/vidmot/StillingarController.java
+++ src/main/java/hi/vidmot/StillingarController.java
@@ -1,26 +1,41 @@
 package hi.vidmot;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class StillingarController {
-    public void saveSettings(ActionEvent actionEvent) {
+
+    @FXML
+    public Label fxVidskiptavinur;
+
+    @FXML
+    public Label fxHeimilisfang;
+
+    @FXML
+    public Label fxGreidslaInfo;
+
+    /* Skoða þetta aðeins betur, þarf að kóða betur
+     */
+    public void initialize() {
+        // setja intialize method hérna inni til að fá info frá viðskiptavin class?
     }
 
-    public void fxVistaStillingarhandler(ActionEvent actionEvent) {
-    }
+        public void fxBreytaStillingar (ActionEvent actionEvent){
+        }
 
-    public void fxTilbakaHandler(ActionEvent actionEvent) throws IOException {
+        public void fxTilbakaHandler (ActionEvent actionEvent) throws IOException {
             Parent parent = FXMLLoader.load(getClass().getResource("pontun-view.fxml"));
             Scene scene = new Scene(parent);
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
             stage.setScene(scene);
             stage.show();
+        }
     }
-}
