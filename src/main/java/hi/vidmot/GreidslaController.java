73d2eab9960a77460da466c8a49abfbc9999a17f
Donn Eunice Bartido Patambag <Donneunice@hotmail.com>
2023-04-20 23:13:03 +0000

    asdf
    asfd

--- src/main/java/hi/vidmot/GreidslaController.java
+++ src/main/java/hi/vidmot/GreidslaController.java
@@ -7,37 +7,25 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.util.Duration;
-import vinnsla.Karfa;
-
-
-/**
- * Byrjum á því að importa öll libraries
- */
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-
 public class GreidslaController {
-    private final Karfa karfa = new Karfa();
 
     public Label fxVidskiptavinur;
     public Label fxHeimilisfang;
-    public ListView fxKarfa;
+    public Label fxBidtimi;
     @FXML
     private Label fxVerd;
     @FXML
     private Button fxStadfestaPontun;
 
     /**
-     * Skilgreinum klasa GreislaController.
-     * Upphafsstilla reglur sem tengja senurnar
+     * upphafsstilla reglur sem tengja senurnar
      */
-
     public void initialize() {
         PontunController pontunController = (PontunController) ViewSwitcher.lookup(View.PONTUN);
-        fxKarfa.setItems(karfa.getVeitingar()); // tengja viðmót og vinnslu
         fxVerd.textProperty().bind(pontunController.getKarfa().heildarVerdProperty().asString());
         fxVidskiptavinur.textProperty().bind(pontunController.getVidskiptavinur().nafnProperty());
         fxHeimilisfang.textProperty().bind(pontunController.getVidskiptavinur().heimilisfangProperty());
@@ -50,20 +38,8 @@ public class GreidslaController {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Pöntun Staðfest");
         alert.setHeaderText(null);
-        alert.setContentText("Yibbí Pöntun staðfest !\n" + "Áætluð afhending NördEats: " + String.format("%02d:%02d", countdownSeconds.get() / 60, countdownSeconds.get() % 60) + " mínutur");
+        alert.setContentText("Yibbí Pöntun staðfest !\n" + "Áætluð afhending NördEats: " + String.format("%02d:%02d", countdownSeconds.get() / 60, countdownSeconds.get() % 60));
         alert.show();
-
-
-        // Búum til tímalínu
-        timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
-            countdownSeconds--;
-            int minutes = countdownSeconds / 60;
-            int seconds = countdownSeconds % 60;
-            fxBidtimi.setText(String.format("%02d:%02d", minutes, seconds));
-            if (countdownSeconds == 0) {
-                timeline.stop(); // Stoppum tímann
-
-                // Bætum kóðanum okkar til að meðhöndla biðtímann
 
         Timeline timeline = null; // Initialize the timeline variable
         Timeline finalTimeline = timeline;
@@ -73,8 +49,7 @@ public class GreidslaController {
                 alert.close();
                 finalTimeline.stop();
             } else {
-                alert.setContentText("Yibbí Pöntun staðfest !\n" + "Áætluð afhending NördEats: " + String.format("%02d:%02d", countdownSeconds.get() / 60, countdownSeconds.get() % 60) + " mínutur");
-
+                alert.setContentText("Yibbí Pöntun staðfest !\n" + "Áætluð afhending NördEats: " + String.format("%02d:%02d", countdownSeconds.get() / 60, countdownSeconds.get() % 60));
             }
         }, new javafx.animation.KeyValue[]{}));
         timeline.setCycleCount(countdownSeconds.get());
